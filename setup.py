430336473dc2534af35fa07f7e9b55e5c183ffd4
Spaceman <frederik.bjorne@gmail.com>
2018-10-10 01:18:55 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'py-observer',
+    version = '1.0.0',
+    url = 'https://github.com/FrederikBjorne/python-observer.git',
+    author = 'Frederik Bjorne',
+    author_email = 'frederik.bjorne@gmail.com’,
+    description = 'Observer pattern implementation',
+    packages = find_packages(),    
+    install_requires = [],
+)
